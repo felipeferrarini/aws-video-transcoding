e4ab43ad1f3043969da41be35a5e9f7215784777
Felipe Ferrarini <ferrarinifelipe@gmail.com>
2022-05-22 11:27:24 -0300

    Add publisher logic

--- src/functions/publisher.ts
+++ src/functions/publisher.ts
@@ -1,12 +1,27 @@
 import { Handler } from "aws-lambda";
+import AWS from "aws-sdk";
+
+const sqs = new AWS.SQS({
+  apiVersion: "latest",
+  region: process.env.AWS_REGION,
+});
 
 export const handler: Handler = async (event, _context) => {
-  console.log(event);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Go Serverless v1.0! Your function executed successfully!",
-      input: event,
-    }),
+  console.log("Received event:", JSON.stringify(event, null, 2));
+
+  // Get the object from the event and show its content type
+  const bucket = event.Records[0].s3.bucket.name;
+  const key = decodeURIComponent(event.Records[0].s3.object.key);
+  const params = {
+    Bucket: bucket,
+    Key: key,
   };
+
+  // Send a message into SQS
+  await sqs
+    .sendMessage({
+      QueueUrl: process.env.QUEUE_URL || "",
+      MessageBody: JSON.stringify(params),
+    })
+    .promise();
 };

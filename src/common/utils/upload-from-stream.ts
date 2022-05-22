e82d567309d1de39d817c6baeebbea3d612bbb08
Felipe Ferrarini <ferrarinifelipe@gmail.com>
2022-05-22 14:09:29 -0300

    Fix file stream upload

--- src/common/utils/upload-from-stream.ts
+++ src/common/utils/upload-from-stream.ts
@@ -4,9 +4,15 @@ import { PassThrough } from "stream";
 export function uploadFromStream(s3: S3, params: S3.PutObjectRequest) {
   var pass = new PassThrough();
 
-  s3.upload(params, (err: Error, data: S3.ManagedUpload.SendData) => {
-    console.log(err, data);
-  });
+  s3.upload(
+    {
+      ...params,
+      Body: pass,
+    },
+    (err: Error, data: S3.ManagedUpload.SendData) => {
+      console.log(err, data);
+    }
+  );
 
   return pass;
 }
